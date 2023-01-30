3fd8874758dca6ebf5249db7bcec87fd703e6f84
Jean-Baptiste Lasselle <jblasselle@michelin.io>
2023-01-30 20:27:37 +0100

    docker compose first test

--- oci/tests/db/mysql/test1.sh
+++ oci/tests/db/mysql/test1.sh
@@ -2,6 +2,7 @@
 
 ramdir="/ramfs"
 sys_tempdir="/tmp"
+mkdir -p ${sys_tempdir}
 sql_client_sql_client_cnf_file_file="$(mktemp "$sys_tempdir/this-script-name-XXXXX")"
 # pw="$(produce_password)"
 export CB_DB_USERNAME="chartbrewuser"
@@ -9,11 +10,29 @@ export CB_DB_PASSWORD="chartbrewpwd"
 export CB_DB_PORT="3306"
 export CB_DB_NAME="chartbrew_db"
 
-cat >"$sql_client_cnf_file" <<EOF
+cat <<EOF >$sql_client_cnf_file
 [client]
 user=${CB_DB_USERNAME}
 password="${CB_DB_PASSWORD}"
 EOF
+echo "# --- # --- # --- # --- # --- # --- # --- #"
+echo "# --- # --- # --- # --- # --- # --- # --- #"
+echo "# --- # --- # --- # "
+echo "# --- # --- # "
+cat -n $sql_client_cnf_file
+echo "# --- # --- # "
+echo "# --- # --- # --- # "
+echo "# --- # --- # --- # --- # --- # --- # --- #"
+# -----------------------------------------
+#
+# cat >"$sql_client_cnf_file" <<EOF
+# [client]
+# user=${CB_DB_USERNAME}
+# password="${CB_DB_PASSWORD}"
+# EOF
+
+
+exit 0
 
 mysql -h 0.0.0.0 --defaults-extra-file="$sql_client_cnf_file" -e 'select 1'
 
